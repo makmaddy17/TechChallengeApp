7dd0474d37d54330715e3ff247d9dc062c98cd26
Mayank Madhur <30777692+makmaddy17@users.noreply.github.com>
2020-09-12 15:00:59 +0530

    Update securitygroup.tf

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -47,3 +47,26 @@ resource "aws_security_group" "myapp-elb-securitygroup" {
     Name = "myapp-elb"
   }
 }
+
+
+resource "aws_security_group" "allow-postgress" {
+  vpc_id      = aws_vpc.main.id
+  name        = "allow-postgress"
+  description = "allow-postgress"
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs-securitygroup.id] # allowing access from our example instance
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
+  }
+  tags = {
+    Name = "allow-postgress"
+  }
+}
